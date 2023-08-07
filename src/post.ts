8049cdfa67cb9ccdc549b971bf2ff2906c092557
vitalii-skrypka <vitaliy.skrypka@techery.io>
2023-08-08 01:02:37 +0200

    post added

---
+++ src/post.ts
@@ -0,0 +1,36 @@
+import fs from 'fs';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { TwitterApi } = require('twitter-api-v2');
+
+import { generateImage } from './imgGenerator';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+/// TWITTER
+const twitterClient = new TwitterApi({
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  appKey: process.env.TWITTER_CONSUMER_KEY,
+  appSecret: process.env.TWITTER_CONSUMER_KEY_SECRET,
+});
+
+/// TWITTER-END
+(async () => {
+  try {
+    await generateImage();
+    const mediaId = await twitterClient.v1.uploadMedia(
+      `/tmp/${process.env.IMAGE_NAME}`
+    );
+    await twitterClient.v2.tweet({
+      media: { media_ids: [mediaId] },
+    });
+    return 0;
+  } catch (error) {
+    console.log(error);
+    return 1;
+  } finally {
+    fs.unlink(`/tmp/${process.env.IMAGE_NAME}`, () =>
+      console.log('File deleted')
+    );
+  }
+})();
